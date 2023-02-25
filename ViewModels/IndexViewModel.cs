a0872f9bf265a92a94cc9721eec456602474a7b5
Patrik Ask <ask.patrik@hotmail.com>
2023-02-25 16:24:39 +0100

    Added viewmodel that matches API

---
+++ ViewModels/IndexViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ministers_of_sweden.web.ViewModels
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Born { get; set; }
+        public string ImageUrl { get; set; }
+   
+    }
+}
