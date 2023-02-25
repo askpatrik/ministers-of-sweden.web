127bbaeae4b445de869686139a27bd183b76eb67
Patrik Ask <ask.patrik@hotmail.com>
2023-02-25 14:58:28 +0100

    Removed ErrorViewModel

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ministers_of_sweden.web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
