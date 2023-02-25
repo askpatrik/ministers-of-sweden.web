9ba90ab117684432551de75d3ed3447f9172e8c9
Patrik Ask <ask.patrik@hotmail.com>
2023-02-25 16:02:16 +0100

    Added HttpClient

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//jag behöver tillgång till denna klassen för att kommunicera med yttervärlden.
+builder.Services.AddHttpClient();
+    
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
