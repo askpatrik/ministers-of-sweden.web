a7b8a8f84ffdc1c44bbb95a83ea44653eb22ea9b
Patrik Ask <ask.patrik@hotmail.com>
2023-02-25 16:24:25 +0100

    Started with Index in controller

--- Controllers/MinistersController.cs
+++ Controllers/MinistersController.cs
@@ -12,5 +12,20 @@ namespace ministers_of_sweden.web.Controllers
             _httpClient = httpClient;
             _baseUrl = config.GetSection("apiSettings:baseUrl").Value;
         }
+
+          public async Task<IActionResult> Index()
+    {
+        using var client = _httpClient.CreateClient();
+        //Nu kan vi göra ett anrop över internet
+        var response = await client.GetAsync($"{_baseUrl}/ministers");
+
+        if(!response.IsSuccessStatusCode) return Content("It went wrong");
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        
+    }   
+
     }
+  
 } 
